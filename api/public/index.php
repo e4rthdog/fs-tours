6062248269912ba22a7c01fef9429120d9a6fcb2
e4rthdog <elias.stassinos@gmail.com>
2025-05-18 21:31:11 +0300

    feat: initialize database connection and update greeting message

--- api/public/index.php
+++ api/public/index.php
@@ -3,12 +3,19 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+use Medoo\Medoo;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$database = new Medoo([
+    'type' => 'sqlite',
+    'database' => __DIR__ . '/../db/fstours.db'
+]);
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Hello world!!");
     return $response;
 });
 
