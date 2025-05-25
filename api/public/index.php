7955017e5ff1832267713645c2bf7d6b886e9cca
e4rthdog <elias.stassinos@gmail.com>
2025-05-25 14:34:36 +0300

    fix: update CORS headers to include X-Requested-With and improve router import formatting

--- api/public/index.php
+++ api/public/index.php
@@ -72,7 +72,7 @@ $app->add(function ($request, $handler) {
   $response = $handler->handle($request);
   return $response
     ->withHeader('Access-Control-Allow-Origin', '*')
-    ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
     ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
 
